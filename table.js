8577a84e60f6100c1d91c13e7742eb206b58fa8b
Hashmi Mohammed <hashmi.smf@gmail.com>
2016-09-11 09:50:13 +0530

    added non commercial and agricultural calculation

--- table.js
+++ table.js
@@ -15,42 +15,40 @@ $('#formaction').submit(function(event){
    agricultural()
   }
   
-  //calculating non commercial bill
-  function noncommercial(){	
-  	var arr = [];
-  	var first_50 = [];
-  	var second_50 = [];
-  	var result = [];
+  //calculation for bill
+  function calculate(r1, r2) {
+    var arr = [];
+    var first_50 = [];
+    var second_50 = [];
+    var result = [];
     var counter = 1
-	  for(i = 1; i <= $units; i++){
-	  	// switch (i) {
-	  	//   case ( i <= 50):
-	  	//     result.push(first_50.push(i * 1.25))
-	  	//     break;	
-	  	//   case ( i > 50 && i <= 100):
-  		//     second_50.push(counter * 1.5)
-    //         counter++
-    //         var concat = first_50.slice(-1).pop() + second_50.slice(-1).pop()
-    //         result.push(concat)
-	  	// }
+    for(i = 1; i <= $units; i++){
         if( i >= 1 && i <= 50){
-           result.push(first_50.push(i * 1.25))
+           first_50.push(i * r1)
+           result.push(first_50.slice(-1)[0])
         }
         if( i > 50 && i <= 100){
-           second_50.push(counter * 1.5)
+           second_50.push(counter * r2)
            counter++
            var concat = first_50.slice(-1).pop() + second_50.slice(-1).pop()
            result.push(concat)
         }
-	  }
+    }
       alert(result.slice(-1).pop());
+  }
+
+  //calculating non commercial bill
+  function noncommercial(){
+    calculate(1.25, 1.5)
+  	
    }
 
   function commercial(){
-
+    calculate(1.5, 1.75)
   }
 
   function agricultural() {
+    calculate(0.75, 1)
   }
 
   event.preventDefault();
