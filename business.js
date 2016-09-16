57fe15712b4fe01c8afe7092bdb382c9c50649ab
shaikmutherpasha <muther178@gmail.com>
2016-09-17 00:30:01 +0530

    using jquery for <tr>

--- business.js
+++ business.js
@@ -1,1 +1,19 @@
-var myArray = [{name: "Muther", address: "Cgutta", branches: 1 },{name:"Hashmi" ,address: "Snagar",branches : 2}]
+var pdetails =[
+{"name": "Muther", address: "Cgutta", "branches": "1" },
+{"name":"Hashmi" ,"address": "Snagar","branches" : "2"}
+];
+
+$.each( pdetails, function( key, value ) {
+  console.log( "key", key, "value", value );
+});
+
+$.each( pdetails, function( i, v ) {
+for (i = 0 ; i < pdetails.length; i++ ) {
+$.appendto("#table1"){	
+ out += "<tr><td>" +
+ pdetails[i].name + "</td><td>" +
+ pdetails[i].address + "</td><td>" +
+ pdetails[i].branches + "</td></tr>"
+ }
+}
+ });
