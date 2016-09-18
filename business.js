d74515562caba75664a82679bf12a72c1a996601
Hashmi Mohammed <hashmi.smf@gmail.com>
2016-09-18 11:20:27 +0530

    added business list and business show

--- business.js
+++ business.js
@@ -1,9 +1,25 @@
-var pdetails =[
-{"name": "Muther", address: "Cgutta", "branches": "1" },
-{"name":"Hashmi" ,"address": "Snagar","branches" : "2"}
+// business objects
+var pDetails = [
+    { "id": 1, "name": "Muther", 'address': "Cgutta", "accounts": "1", job: 'front-end' },
+    { "id": 2, "name": "Hashmi", "address": "Snagar", "accounts": "2", job: 'full-stack' },
+    { "id": 3, "name": "Nazeem", "address": "kukatpally", "accounts": "2", job: 'web-desinger' }
 ];
 
-$.each(pdetails, function( key, value ) {
-  $('#table1').html('<tr><td>' + pdetails[0].name + '</td>' + '<td>' + pdetails[0].address + '</td>' + '<td>' + pdetails[0].branches + '</td></tr>'  );
+// business listing
+$.each(pDetails, function(key, value) {
+    $('#detailList').append('<tr id= ' + value.id + ' class= "details"> <td>' + value.name + '</td>' +
+        '<td>' + value.address + '</td>' +
+        '<td>' + value.accounts + '</td></tr>'
+    );
 });
 
+// business show
+$('.details').click(function() {
+    var id = $(this).attr('id')
+    var persnDetl = pDetails.find(x => x.id == id)
+    $('.panel-heading > .panel-title').text('Personal Information')
+    $('#business-show').toggle();
+    $('#business-list').toggle();
+})
+
+$('#business-show').hide();
